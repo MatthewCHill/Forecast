3465d6e80c8ed32545e1f409c96e79bb9216a89d
Matthew Hill <matthill265@gmail.com>
2023-02-21 15:55:12 -0700

    completed the fetch function and the networking code required to hit the API Endpoint

--- Forecast/Forecast/Controller/DayController.swift
+++ Forecast/Forecast/Controller/DayController.swift
@@ -24,5 +24,28 @@ class DayController {
         guard let finalURL = urlComponents?.url else {completion(nil); return}
         print(finalURL)
         
+        URLSession.shared.dataTask(with: finalURL) { data, response, error in
+            
+            if let error = error {
+                print(error.localizedDescription)
+                completion(nil)
+                return
+            }
+            if let response = response as? HTTPURLResponse {
+                print(response.statusCode)
+            }
+            guard let data = data else { completion(nil); return}
+            
+            do {
+                if let topLevel = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed) as? [String : Any], let dataArray = topLevel["data"] as? [[String: Any]] {
+                
+                    let cityWeather = dataArray.compactMap { Day(dictionary: $0, cityName: "city_name") }
+                }
+            } catch {
+                print("Error in Do/Try/Catch: \(error.localizedDescription)")
+                completion(nil)
+                return
+            }
+        } .resume()
     }
 }
